7e425e651e52a843eb393c1e9a44e74ba5169e6a
000 <000@000-PC>
2019-02-02 00:10:55 +0300

    first test

---
+++ name/src/name/MyNam.java
@@ -0,0 +1,8 @@
+package name;
+
+public class MyNam {
+public static void main(String[]args){
+	System.out.println("Amani Zaqzouq");
+}
+
+}
---
+++ name/src/name/myName.java
@@ -0,0 +1,8 @@
+package name;
+
+public class myName {
+public static void main(String args[]){
+	System.out.println("Amani");
+}
+
+}
